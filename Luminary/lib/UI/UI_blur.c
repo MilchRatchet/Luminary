c7f5dcc27407aecf0bb1d19b4ca079d861f97a81
MilchRatchet <milchratchet@googlemail.com>
2021-08-03 17:22:29 +0200

    background blur is now bilinearly upscaled

--- Luminary/lib/UI/UI_blur.c
+++ Luminary/lib/UI/UI_blur.c
@@ -8,24 +8,33 @@
 #define BLUR_HEIGHT (UI_HEIGHT + UI_BORDER_SIZE) / SCALE
 
 #define SIGMA 3.0f
-#define STRENGTH (8.0f)
+#define STRENGTH 8.0f
 #define KERNEL_SIZE ((int) (SIGMA * 3.0f) + 1)
+
+#define SHIFT 11
+#define FACTOR 2048
 
 void blur_background(UI* ui, uint8_t* target, int width, int height) {
   const int off = 4 * width * ui->y + 4 * ui->x;
   float* temp   = (float*) ui->scratch;
 
-  float* gauss_kernel         = temp + (BLUR_HEIGHT * BLUR_WIDTH * 3);
-  uint8_t* target_convolution = (uint8_t*) (gauss_kernel + KERNEL_SIZE);
+  float* gauss_kernel = temp + (BLUR_HEIGHT * BLUR_WIDTH * 3);
+  uint8_t* internal   = (uint8_t*) (gauss_kernel + KERNEL_SIZE);
 
   for (int i = 0; i < BLUR_HEIGHT; i++) {
     for (int j = 0; j < BLUR_WIDTH; j++) {
-      target_convolution[i * BLUR_WIDTH * 3 + j * 3] =
-        target[off + SCALE * i * width * 4 + 4 * SCALE * j + 2];
-      target_convolution[i * BLUR_WIDTH * 3 + j * 3 + 1] =
-        target[off + SCALE * i * width * 4 + 4 * SCALE * j + 1];
-      target_convolution[i * BLUR_WIDTH * 3 + j * 3 + 2] =
-        target[off + SCALE * i * width * 4 + 4 * SCALE * j];
+      internal[i * BLUR_WIDTH * 3 + j * 3] =
+        (((int) target[off + 4 * (SCALE * i * width + SCALE * j) + 2]
+          + (int) target[off + 4 * ((SCALE * i + SCALE / 2) * width + (SCALE * j + SCALE / 2)) + 2])
+         >> 1);
+      internal[i * BLUR_WIDTH * 3 + j * 3 + 1] =
+        (((int) target[off + 4 * (SCALE * i * width + SCALE * j) + 1]
+          + (int) target[off + 4 * ((SCALE * i + SCALE / 2) * width + (SCALE * j + SCALE / 2)) + 1])
+         >> 1);
+      internal[i * BLUR_WIDTH * 3 + j * 3 + 2] =
+        (((int) target[off + 4 * (SCALE * i * width + SCALE * j)]
+          + (int) target[off + 4 * ((SCALE * i + SCALE / 2) * width + (SCALE * j + SCALE / 2))])
+         >> 1);
     }
   }
 
@@ -44,15 +53,15 @@ void blur_background(UI* ui, uint8_t* target, int width, int height) {
       const int end   = min(KERNEL_SIZE, BLUR_HEIGHT - i);
 
       for (int k = -KERNEL_SIZE + 1; k < start; k++) {
-        collector += gauss_kernel[abs(k)] * target_convolution[(i + start) * BLUR_WIDTH * 3 + j];
+        collector += gauss_kernel[abs(k)] * internal[(i + start) * BLUR_WIDTH * 3 + j];
       }
 
       for (int k = start; k < end; k++) {
-        collector += gauss_kernel[abs(k)] * target_convolution[(i + k) * BLUR_WIDTH * 3 + j];
+        collector += gauss_kernel[abs(k)] * internal[(i + k) * BLUR_WIDTH * 3 + j];
       }
 
       for (int k = end; k < KERNEL_SIZE; k++) {
-        collector += gauss_kernel[abs(k)] * target_convolution[(i + end - 1) * BLUR_WIDTH * 3 + j];
+        collector += gauss_kernel[abs(k)] * internal[(i + end - 1) * BLUR_WIDTH * 3 + j];
       }
 
       temp[i * BLUR_WIDTH * 3 + j] = fminf(255.0f, collector);
@@ -78,18 +87,45 @@ void blur_background(UI* ui, uint8_t* target, int width, int height) {
         collector += gauss_kernel[abs(k)] * temp[i * BLUR_WIDTH * 3 + (j + (end - 1) * 3)];
       }
 
-      target_convolution[i * BLUR_WIDTH * 3 + j] = fminf(255.0f, collector);
+      internal[i * BLUR_WIDTH * 3 + j] = fminf(255.0f, collector);
     }
   }
 
+  const int scale_x = (int) ((double) FACTOR * (BLUR_WIDTH - 1) / UI_WIDTH + 0.5);
+  const int scale_y =
+    (int) ((double) FACTOR * (BLUR_HEIGHT - 1) / (UI_HEIGHT + UI_BORDER_SIZE) + 0.5);
+
   for (int i = 0; i < UI_HEIGHT + UI_BORDER_SIZE; i++) {
+    const int source_y = i * scale_y;
+    int y0             = source_y >> SHIFT;
+    const int fy       = source_y - (y0 << SHIFT);
+    const int ify      = FACTOR - fy;
+
     for (int j = 0; j < UI_WIDTH; j++) {
-      target[off + i * width * 4 + 4 * j + 2] =
-        target_convolution[(i / SCALE) * BLUR_WIDTH * 3 + 3 * (j / SCALE)];
-      target[off + i * width * 4 + 4 * j + 1] =
-        target_convolution[(i / SCALE) * BLUR_WIDTH * 3 + 3 * (j / SCALE) + 1];
-      target[off + i * width * 4 + 4 * j] =
-        target_convolution[(i / SCALE) * BLUR_WIDTH * 3 + 3 * (j / SCALE) + 2];
+      const int source_x = j * scale_x;
+      int x0             = source_x >> SHIFT;
+      const int fx       = source_x - (x0 << SHIFT);
+      const int ifx      = FACTOR - fx;
+
+      const int index0 = 3 * (y0 * BLUR_WIDTH + x0);
+      const int index1 = 3 * (y0 * BLUR_WIDTH + x0 + 1);
+      const int index2 = 3 * ((y0 + 1) * BLUR_WIDTH + x0);
+      const int index3 = 3 * ((y0 + 1) * BLUR_WIDTH + x0 + 1);
+
+      target[off + i * width * 4 + 4 * j + 2] = (uint8_t)(
+        (internal[index0] * ifx * ify + internal[index1] * fx * ify + internal[index2] * ifx * fy
+         + internal[index3] * fx * fy + (FACTOR * FACTOR / 2))
+        >> (2 * SHIFT));
+
+      target[off + i * width * 4 + 4 * j + 1] = (uint8_t)(
+        (internal[index0 + 1] * ifx * ify + internal[index1 + 1] * fx * ify
+         + internal[index2 + 1] * ifx * fy + internal[index3 + 1] * fx * fy + (FACTOR * FACTOR / 2))
+        >> (2 * SHIFT));
+
+      target[off + i * width * 4 + 4 * j] = (uint8_t)(
+        (internal[index0 + 2] * ifx * ify + internal[index1 + 2] * fx * ify
+         + internal[index2 + 2] * ifx * fy + internal[index3 + 2] * fx * fy + (FACTOR * FACTOR / 2))
+        >> (2 * SHIFT));
     }
   }
 }
