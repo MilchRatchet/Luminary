2c2e0fd36f4c01694dbab4e89c49bfe2245bf0fd
MilchRatchet <milchratchet@googlemail.com>
2021-10-09 17:13:13 +0200

    Improving Fog.

--- Luminary/lib/cuda/fog.cuh
+++ Luminary/lib/cuda/fog.cuh
@@ -29,9 +29,35 @@ __device__ float get_intersection_fog(vec3 origin, vec3 ray, float random) {
 
   max_dist = fminf(device_scene.fog.dist, max_dist);
 
-  float t = logf(random) / (-device_scene.fog.scattering_coeff * 0.00001f);
+  float t = fmaxf(0.0f, min_dist) + logf(random) / (-device_scene.fog.scattering_coeff * 0.00001f);
 
   return (t < min_dist || t > max_dist) ? FLT_MAX : t;
+}
+
+__device__ float get_fog_depth(float y, float ry, float depth) {
+  float height = device_scene.fog.height + device_scene.fog.falloff;
+
+  if (y >= height && ry >= 0.0f)
+    return 0.0f;
+
+  if (y < height && ry <= 0.0f)
+    return fminf(device_scene.fog.dist, depth);
+
+  if (y < height) {
+    return fminf(device_scene.fog.dist, fminf(((height - y) / ry), depth));
+  }
+
+  return fmaxf(0.0f, fminf(device_scene.fog.dist, depth - ((height - y) / ry)));
+}
+
+__device__ float get_fog_density(float base_density, float height) {
+  if (height > device_scene.fog.height) {
+    base_density = (height < device_scene.fog.height + device_scene.fog.falloff)
+                     ? lerp(base_density, 0.0f, (height - device_scene.fog.height) / device_scene.fog.falloff)
+                     : 0.0f;
+  }
+
+  return base_density;
 }
 
 #endif /* CU_FOG_H */
