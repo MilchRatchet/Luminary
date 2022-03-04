93060571da52a3712eeb5cfde906f4419df28462
MilchRatchet <milchratchet@googlemail.com>
2022-03-04 21:57:26 +0100

    Revised performance directives.

--- Luminary/lib/cuda/directives.cuh
+++ Luminary/lib/cuda/directives.cuh
@@ -1,24 +1,26 @@
 #ifndef CU_DIRECTIVES_H
 #define CU_DIRECTIVES_H
 
+#include "utils.cuh"
+
 /*
  * Define WEIGHT_BASED_EXIT for the following to apply
  *
  * Rays with an accumulated weight below CUTOFF are cancelled
  * Higher values provide better performance at the cost of extreme bright lights not being shaded properly
  *
- * Change BRIGHTEST_EMISSION to the intensity of the brightest light for best performance without
- * visual degradation
- *
  * Rays with an accumulated weight below PROBABILISTIC_CUTOFF have a chance equal to
  * the inverse linear interpolation of the weight and CUTOFF and PROBABILISTIC_CUTOFF
  * Higher values provide much better performance at the cost of noisy/too dark areas
  * Setting PROBABILISTIC_CUTOFF >= CUTOFF will deactivate this option
+ *
+ * Tests show a significant performance increase with very minimal visual impact. The visuals
+ * are perceptively unaffected.
  */
 #define WEIGHT_BASED_EXIT
-#define BRIGHTEST_EMISSION 40.0f
-#define CUTOFF ((1.0f) / (BRIGHTEST_EMISSION * 255.0f))
-#define PROBABILISTIC_CUTOFF ((1.0f) / (0.5f * BRIGHTEST_EMISSION * 255.0f))
+#define BRIGHTEST_EMISSION (device_scene.camera.exposure * fmaxf(device_scene.sky.sun_strength, device_scene.material.default_material.b))
+#define CUTOFF ((1.0) / (BRIGHTEST_EMISSION))
+#define PROBABILISTIC_CUTOFF ((1.0f) / (0.25f * BRIGHTEST_EMISSION))
 
 /*
  * Define LOW_QUALITY_LONG_BOUNCES for the following to apply
@@ -27,8 +29,10 @@
  * This provides much better performance at the cost of noisy indirectly lit areas
  *
  * MIN_BOUNCES controls the minimum amount of bounces that have to happen before this may happen
+ *
+ * Tests show that this significantly darkens any GI contribution. This option is thus not advisable.
  */
-#define LOW_QUALITY_LONG_BOUNCES
+//#define LOW_QUALITY_LONG_BOUNCES
 #define MIN_BOUNCES 1
 
 /*
@@ -36,6 +40,8 @@
  *
  * A sample exits early when light information have already been gathered
  * This improves performance at the cost of reflections in directly lit surfaces
+ *
+ * This option causes artifacts and is thus deprecated.
  */
 //#define SINGLE_CONTRIBUTION_ONLY
 
