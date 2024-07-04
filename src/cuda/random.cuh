45f1fe4a2e796de9f2514c307c5dd56f8092c67b
MilchRatchet <milchratchet@googlemail.com>
2024-07-04 20:12:15 +0200

    Fixed an issue with how light indices are sampled.
    Off by one mistake, fuck me sideways.

--- src/cuda/random.cuh
+++ src/cuda/random.cuh
@@ -175,8 +175,13 @@ __device__ float random_uint16_t_to_float(const uint16_t v) {
 ////////////////////////////////////////////////////////////////////
 
 // Integer fractions of the actual numbers
+#define R1_PHI1 2654435769u /* 0.61803398875f */
 #define R2_PHI1 3242174888u /* 0.7548776662f  */
 #define R2_PHI2 2447445413u /* 0.56984029f    */
+
+__device__ uint32_t random_r1(const uint32_t offset) {
+  return offset * R1_PHI1;
+}
 
 // [Rob18]
 __device__ uint2 random_r2(const uint32_t offset) {
--- src/cuda/ris.cuh
+++ src/cuda/ris.cuh
@@ -92,7 +92,7 @@ __device__ uint32_t ris_sample_light(
   for (int i = 0; i < reservoir_size; i++) {
     uint32_t id_rand = quasirandom_sequence_1D_base(
       QUASI_RANDOM_TARGET_RIS_LIGHT_ID + light_ray_index * reservoir_size + i, pixel, device.temporal_frames, device.depth);
-    uint32_t presampled_id = id_rand >> (31 - device.ris_settings.light_candidate_pool_size_log2);
+    uint32_t presampled_id = id_rand >> (32 - device.ris_settings.light_candidate_pool_size_log2);
     uint32_t id            = device.ptrs.light_candidates[presampled_id];
 
     if (id == blocked_light_id)
@@ -164,8 +164,7 @@ LUMINARY_KERNEL void ris_presample_lights() {
 
   const int light_sample_bin_count = 1 << device.ris_settings.light_candidate_pool_size_log2;
   while (id < light_sample_bin_count) {
-    const uint32_t sampled_id =
-      quasirandom_sequence_1D_base(id, make_ushort2(0, 0), device.temporal_frames, 0) % device.scene.triangle_lights_count;
+    const uint32_t sampled_id = random_r1(light_sample_bin_count * device.temporal_frames + id) % device.scene.triangle_lights_count;
 
     device.ptrs.light_candidates[id]                   = sampled_id;
     device.ris_settings.presampled_triangle_lights[id] = device.scene.triangle_lights[sampled_id];
