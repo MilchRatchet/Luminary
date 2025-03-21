1e01605570f6238176150c8e11140ef6a30fc8d6
MilchRatchet <milchratchet@googlemail.com>
2025-03-21 15:00:56 +0100

    Bumped licence year.

--- include/luminary/wall_time.h
+++ include/luminary/wall_time.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2021-2024 Max Jenke
+  Copyright (C) 2021-2025 Max Jenke
 
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU Affero General Public License as published
