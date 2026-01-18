c0c76a021f8dfd2cec00ebaa8b8d0e0c2608a892
MilchRatchet <milchratchet@googlemail.com>
2026-01-18 14:37:20 +0100

    Fixed crash when disabling adaptive sampling.

--- src/luminary/device/cuda/adaptive_sampling.cuh
+++ src/luminary/device/cuda/adaptive_sampling.cuh
@@ -75,18 +75,19 @@ LUMINARY_FUNCTION uint32_t adapative_sampling_get_sample_offset(const uint32_t x
 }
 
 LUMINARY_FUNCTION uint32_t adaptive_sampling_get_sample_count_from_block_index(const uint32_t adaptive_sampling_block) {
-  const uint32_t adaptive_sampling_counts = device.ptrs.stage_sample_counts[adaptive_sampling_block];
+  const uint32_t this_stage_id            = device.state.sample_allocation.stage_id;
+  const uint32_t adaptive_sampling_counts = (this_stage_id > 0) ? device.ptrs.stage_sample_counts[adaptive_sampling_block] : 0;
 
   uint32_t count = device.state.adaptive_sampling_accumulated_stages[0];
 
-  for (uint32_t stage_id = 0; stage_id < ADAPTIVE_SAMPLER_NUM_STAGES; stage_id++) {
+  LUMINARY_ASSUME(this_stage_id <= ADAPTIVE_SAMPLER_NUM_STAGES);
+
+  for (uint32_t stage_id = 0; stage_id < this_stage_id; stage_id++) {
     const uint32_t stage_sample_offset = device.state.adaptive_sampling_accumulated_stages[stage_id + 1];
     const uint32_t stage_sample_count  = adaptive_sampling_get_stage_sample_count(adaptive_sampling_counts, stage_id);
 
     count += stage_sample_offset * stage_sample_count;
   }
-
-  const uint32_t this_stage_id = device.state.sample_allocation.stage_id;
 
   if (this_stage_id > 0) {
     count += ((adaptive_sampling_counts >> ((this_stage_id - 1) * 8)) & 0xFF) + 1;
