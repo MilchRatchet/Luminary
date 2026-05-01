e0ac83a0cfe71a374322002015e521c0d1b072b7
MilchRatchet <milchratchet@googlemail.com>
2026-05-01 12:46:25 +0200

    Implemented ray transfer matrix analysis for physical camera.

--- src/luminary/device/device_physical_camera.c
+++ src/luminary/device/device_physical_camera.c
@@ -15,15 +15,65 @@ LuminaryResult physical_camera_create(PhysicalCamera** physical_camera) {
   return LUMINARY_SUCCESS;
 }
 
+struct RayTransferMatrix {
+  float A;
+  float B;
+  float C;
+  float D;
+} typedef RayTransferMatrix;
+
+static RayTransferMatrix _physical_camera_run_ray_transfer_matrix_analysis(const LensTemplateData* template_data) {
+  float A = 1.0f;
+  float B = 0.0f;
+  float C = 0.0f;
+  float D = 1.0f;
+
+  float z_curr = -template_data->last_vertex;
+
+  for (int32_t i = template_data->num_interfaces - 1; i >= 0; i--) {
+    float z_v = -template_data->interfaces[i].vertex;
+    float d   = z_v - z_curr;
+
+    float new_A = A + d * C;
+    float new_B = B + d * D;
+    A           = new_A;
+    B           = new_B;
+
+    z_curr = z_v;
+
+    // media[i+1] is before the interface when travelling backwards from the scene
+    float n1 = template_data->media[i + 1].design_ior;
+    float n2 = template_data->media[i].design_ior;
+    float r  = template_data->interfaces[i].radius;
+
+    if (r != FLT_MAX && r != 0.0f) {
+      float M21 = (n1 - n2) / (r * n2);
+      float M22 = n1 / n2;
+
+      float new_C = M21 * A + M22 * C;
+      float new_D = M21 * B + M22 * D;
+      C           = new_C;
+      D           = new_D;
+    }
+    else {
+      float M22 = n1 / n2;
+      C         = M22 * C;
+      D         = M22 * D;
+    }
+  }
+
+  return (RayTransferMatrix) {.A = A, .B = B, .C = C, .D = D};
+}
+
 static float _physical_camera_compute_auto_focus(
-  float focal_length, float object_distance, float last_vertex, float front_principal_point, float back_principal_point) {
-  // Subtract the last vertex again because we center the simulation space on the last vertex at the end.
-  const float f = focal_length;
-  const float o = object_distance * CAMERA_COMMON_INV_SCALE + last_vertex - front_principal_point;
+  const LensTemplateData* template_data, const RayTransferMatrix matrix, float object_distance) {
+  const float d_o = object_distance * CAMERA_COMMON_INV_SCALE;
 
-  const float i = (f * o) / (o - f);
+  // Standard focus condition from an object at d_o hitting a transfer matrix:
+  const float d_i = -(matrix.A * d_o + matrix.B) / (matrix.C * d_o + matrix.D);
 
-  const float sensor_distance = i - back_principal_point;
+  // d_i is the distance past the final traced interface (interfaces[0])
+  const float sensor_distance = d_i - template_data->interfaces[0].vertex;
 
   return sensor_distance;
 }
@@ -49,26 +99,36 @@ LuminaryResult physical_camera_generate(PhysicalCamera* physical_camera, const C
 
   float sensor_distance = 1.0f;
 
-  if (camera->lens_template != LUMINARY_LENS_TEMPLATE_THIN_LENS) {
-    if (camera->lens.use_auto_focus) {
-      sensor_distance = _physical_camera_compute_auto_focus(
-        template_data.design_focal_length, camera->lens.object_distance / camera->scale, template_data.last_vertex,
-        template_data.front_principal_plane, template_data.back_principal_plane);
-    }
-    else {
-      sensor_distance = camera->lens.sensor_distance;
-    }
-  }
-
   float aperture_radius = 0.0f;
   if (camera->lens.aperture_stop < 32.0f * 1024.0f)
     aperture_radius = (template_data.design_focal_length / camera->lens.aperture_stop) * 0.5f;
 
+  float exit_pupil_radius = template_data.exit_pupil_diameter * 0.5f;
+  float exit_pupil_point  = template_data.exit_pupil_point;
+
+  if (camera->lens_template != LUMINARY_LENS_TEMPLATE_THIN_LENS) {
+    const RayTransferMatrix matrix = _physical_camera_run_ray_transfer_matrix_analysis(&template_data);
+
+    if (camera->lens.use_auto_focus) {
+      sensor_distance = _physical_camera_compute_auto_focus(&template_data, matrix, camera->lens.object_distance / camera->scale);
+    }
+    else {
+      sensor_distance = camera->lens.sensor_distance;
+    }
+
+    if (matrix.D != 0.0f) {
+      const float matrix_determinant = matrix.A * matrix.D - matrix.B * matrix.C;
+
+      exit_pupil_point  = matrix.B / matrix.D;
+      exit_pupil_radius = fabsf(matrix_determinant / matrix.D) * aperture_radius;
+    }
+  }
+
   physical_camera->num_interfaces    = template_data.num_interfaces;
   physical_camera->aperture_radius   = aperture_radius;
   physical_camera->aperture_point    = template_data.aperture_point;
-  physical_camera->exit_pupil_point  = template_data.exit_pupil_point;
-  physical_camera->exit_pupil_radius = template_data.exit_pupil_diameter * 0.5f;
+  physical_camera->exit_pupil_point  = exit_pupil_point;
+  physical_camera->exit_pupil_radius = exit_pupil_radius;
   physical_camera->last_vertex       = template_data.last_vertex;
   physical_camera->sensor_distance   = sensor_distance;
 
